ef3a3e289e466c8cb248f712c4fed9480c65fbfc
titouanlebot-sys <titouan.lebot@gmail.com>
2026-04-27 18:08:47 +0200

    Update IMC.java

--- IMC.java
+++ IMC.java
@@ -1,1 +1,44 @@
 
+
+import java.util.Scanner;
+
+/**
+ * le but de cette class est de
+ */
+public class IMC {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("quelle est votre poids : ");
+        float poids = sc.nextFloat();
+        System.out.println("quelle est votre taille : ");
+        float taille = sc.nextFloat()/100;
+        double tailleCarre = Math.pow(taille, 2);
+
+        double imc  = poids / tailleCarre;
+        System.out.println("votre imc est de "+ imc);
+        if (imc<18.5){
+            System.out.println("vous etes en état de Maigreur");
+        }
+        else if (imc>18.5 && imc<25) {
+            System.out.println("vous etes normal");
+        }
+        else if (imc>25 && imc<30) {
+            System.out.println("vous etes en surpoids");
+        }
+        else{
+            System.out.println("vous etes en état d'obésité");
+        }
+
+
+
+        sc.close();
+
+
+    }
+
+
+
+}
+
